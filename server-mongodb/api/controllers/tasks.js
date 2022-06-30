55efa8aef0c1da2b63eae5616bafb510f125fbbf
sigltech <101203107+sigltech@users.noreply.github.com>
2022-06-30 23:46:32 +0100

    trying to fix authentication and verification

--- server-mongodb/api/controllers/tasks.js
+++ server-mongodb/api/controllers/tasks.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-
+const dotenv = require('dotenv').config();
 const Task = require("../models/task");
 
 router.get("/", verifyToken, async (req, res) => {

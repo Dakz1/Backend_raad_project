f157c67fdab9580b0f12248711de8f25b121cca8
Dakshitha Mak <dakshitha1992@hotmail.com>
2022-07-01 11:05:05 +0100

    deleted tests

--- server-mongodb/api/__test__/Integration/dbmongo.test.js
+++
@@ -1,1 +0,0 @@
-// const dbMongo = require('./jest.config')
--- server-mongodb/api/database.js
+++ server-mongodb/api/database.js
@@ -5,7 +5,7 @@ let mongod = null;
 
 const connectDB = async () => {
   try {
-    let dbUrl = process.env.DB_CONNECTION || "mongodb://localhost:27017/shelter";
+    let dbUrl = "mongodb://localhost:27017/shelter";
     if (process.env.NODE_ENV === 'test') {
       mongod = await MongoMemoryServer.create();
       dbUrl = mongod.getUri();
